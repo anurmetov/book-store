1281f6d94843123f0b13d5f892de6065d359c312
anurmetov <artemnurmetow@gmail.com>
2026-07-17 16:54:29 +0200

    implemented task requirments

---
+++ src/main/java/mate/academy/SpringRepoApplication.java
@@ -0,0 +1,34 @@
+package mate.academy;
+
+import java.math.BigDecimal;
+import mate.academy.entity.Book;
+import mate.academy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(scanBasePackages = "mate.academy")
+public class SpringRepoApplication {
+
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRepoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book bookEntity = new Book();
+            bookEntity.setAuthor("Author");
+            bookEntity.setTitle("Hi");
+            bookEntity.setIsbn("!");
+            bookEntity.setPrice(BigDecimal.ONE);
+            bookService.save(bookEntity);
+        };
+    }
+}
+
--- src/main/java/mate/academy/repository/BookRepository.java
+++ src/main/java/mate/academy/repository/BookRepository.java
@@ -1,8 +1,14 @@
 package mate.academy.repository;
 
+import java.util.List;
+import mate.academy.entity.Book;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository {
 
+    Book save(Book book);
+
+    List findAll();
 }
+
