d9f085c2f14f380641bb0f18a49bfef99b7353b9
artem <artemnurmetow@gmail.com>
2026-08-11 21:53:16 +0200

    - code checkstyle refacoring and dto modifying

--- src/main/java/mate/academy/dto/BookDto.java
+++ src/main/java/mate/academy/dto/BookDto.java
@@ -1,8 +1,10 @@
 package mate.academy.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class BookDto {
     private Long id;
     private String title;
