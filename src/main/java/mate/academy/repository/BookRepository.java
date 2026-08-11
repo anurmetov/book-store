8069365eab281af39d0e8d9575038abcce7fe50f
artem <artemnurmetow@gmail.com>
2026-08-11 20:28:18 +0200

    - migration from manual CRUD implementaion to JPA Repository implementation

--- src/main/java/mate/academy/repository/BookRepository.java
+++ src/main/java/mate/academy/repository/BookRepository.java
@@ -3,10 +3,11 @@ package mate.academy.repository;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book save(Book book);
 
