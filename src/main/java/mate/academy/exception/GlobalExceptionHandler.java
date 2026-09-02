12bf65f1ad967d20d7b8f568a5b2cd2d635232bc
artem <artemnurmetow@gmail.com>
2026-09-02 22:32:56 +0200

    - implemented global exception handler logic and added 3 methods to validate requests

--- src/main/java/mate/academy/exception/GlobalExceptionHandler.java
+++ src/main/java/mate/academy/exception/GlobalExceptionHandler.java
@@ -1,24 +1,61 @@
 package mate.academy.exception;
 
-
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.apache.commons.collections4.map.LinkedMap;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-    //isbn exists already. isbn must be unique -> SQL Exception (.SQLIntegrityConstraintViolationException
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
-    //notNull values -> SQL Exception (.SQLIntegrityConstraintViolationException)
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    protected ResponseEntity<Object> handleUniqueValuesError(SQLIntegrityConstraintViolationException ex) {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("status", HttpStatus.BAD_REQUEST);
+        responseBody.put("errors", ex.getMessage());
 
-    // Book wasnt found -> EntityNotFoundExceptrion
+        return ResponseEntity.badRequest().body(responseBody);
+    }
 
-    // price not null
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException entityNotFoundException) {
+
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("status", HttpStatus.NOT_FOUND);
+        responseBody.put("errors", entityNotFoundException.getMessage());
+
+        return ResponseEntity.badRequest().body(responseBody);
+    }
 
 
+    @Nullable
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("status", HttpStatus.BAD_REQUEST);
+        responseBody.put("errors", ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + " " + err.getDefaultMessage())
+                .toList());
 
-
-
-
-
-
+        return ResponseEntity.badRequest().body(responseBody);
+    }
 }
